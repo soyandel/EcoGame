086ad2db5c92253802c99afc53e299ff2f30221f
soyandel <145588585+soyandel@users.noreply.github.com>
2023-09-26 20:33:16 -0300

    aaa2

--- EcoGame/EcoGame/views.py
+++ EcoGame/EcoGame/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
-def t2(request): #Nuestra primera vista, devuelve lo que está en el return
+def intento(request): #Nuestra primera vista, devuelve lo que está en el return
 
     return HttpResponse("wuiiiii")
