7296582738701960e0ffcd5be554b27f0caf3c5f
soyanedel <soyanedel@usm.cl>
2023-09-26 22:41:16 -0300

    dios nos salve

--- EcoGame/EcoGame/settings.py
+++ EcoGame/EcoGame/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "eco_app",
 ]
 
 MIDDLEWARE = [
--- EcoGame/EcoGame/views.py
+++ EcoGame/EcoGame/views.py
@@ -7,3 +7,4 @@ def intento(request): #Nuestra primera vista, devuelve lo que está en el return
 def datoss(request):
 
     return HttpResponse("Equipo 10 / Equipo Alfa buena maravilla onda dinamita escuadrón lobo")
+
