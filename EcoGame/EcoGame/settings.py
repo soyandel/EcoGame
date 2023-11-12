55662d3a3420a83c5299f43b4d1bcfc17c9bb706
vichoxps <vichoxps@gmail.com>
2023-11-12 16:06:12 -0300

    aaaa

--- EcoGame/EcoGame/settings.py
+++ EcoGame/EcoGame/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = "EcoGame.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "Ecogame",
-        "USER": "postgres",
-        "PASSWORD": "Ecogame-10",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Ecogame',
+        'USER': 'postgres',
+        'PASSWORD': 'Ecogame-10',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
---
+++ EcoGame/eco_app/migrations/__init__.py
@@ -0,0 +1,2 @@
+import psycopg2
+conn = psycopg2.connect(database="Ecogame", user="postgres", password="Ecogame-10")
