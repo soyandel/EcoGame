c04bf2a9a8347f08ace6fbdb053d03d7ac192e81
vichoxps <vichoxps@gmail.com>
2023-11-28 20:23:11 -0300

    cambio minimo

--- EcoGame/eco_app/Inicio_de_sesion/login.php
+++ EcoGame/eco_app/Inicio_de_sesion/login.php
@@ -4,7 +4,7 @@ session_start();
 $usuario=$_POST['txtusuario'];
 $pass=$_POST['txtpass'];
 
-$query="SELECT * FROM usuarios WHERE usuario='$usuario' AND contrasena='$pass'"
+$query="SELECT * FROM usuarios WHERE usuario='$usuario' AND contrasena='$pass'";
 $consulta=pg_query($conexion,$query);
 $cantidad=pg_num_rows($consulta);
 if($cantidad>0){
